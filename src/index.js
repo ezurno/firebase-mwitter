e82668ab5bc676ae378b214cdd307cc945470de0
ezurno <ezurno@gmail.com>
2023-05-14 18:47:15 +0900

    SET: connect firebase
    1. firebase 에 접속 및 로그인
    
    2. 프로젝트 생성 후 npm install firebase > firebase file 생성 및 스크립트 붙여넣기 (api-key)
    
    3. .env 로 hide

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import firebase from "./firebase";
+
+console.log(firebase);
+// firebase가 잘 연결 되어 있는지 확인하기 위함
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
