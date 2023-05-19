dd79bb99de6cef508249cfa8b33e0dbe061c00f3
ezurno <ezurno@gmail.com>
2023-05-19 20:42:17 +0900

    ADD: create login-form
    1. firebase 에서 authentication 설정
    
    2. github, google 으로도 로그인 연결

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
 import { useState } from "react";
 import Router from "components/Router";
-import fbase from "fbase";
 import { authService } from "fbase";
 
 export default function App() {
   //   console.log(authService.currentUser);
   // authService 의 currentUser var 는 현재 유저정보를 불러옴 User | null
 
-  const [isLogin, setIsLogin] = useState(authService.currentUser);
+  const [isLogin, setIsLogin] = useState(authService.currentUser || false);
   return (
     <>
       <Router isLogin={isLogin} />
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,3 +1,50 @@
+import React, { useState } from "react";
+
 export default function Auth() {
-  return <span>Auth</span>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input type="submit" value="Log In" />
+      </form>
+
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with GitHub</button>
+      </div>
+    </div>
+  );
 }
