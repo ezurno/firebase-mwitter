c72e21239fedd1ac301c5126cbff313618e49e28
ezurno <ezurno@gmail.com>
2023-05-19 21:23:13 +0900

    SET: reformed login-form
    1. react-hook-form 을 사용해 수정
    
    2. 간단하게 state 관리도 가능

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import firebase from "./fbase";
 import App from "./components/App";
 
-console.log(firebase);
+// console.log(firebase);
 // firebase가 잘 연결 되어 있는지 확인하기 위함
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,50 +1,117 @@
-import React, { useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { useForm } from "react-hook-form";
 
 export default function Auth() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
 
-  const onChange = (event) => {
-    const {
-      target: { name, value },
-    } = event;
-    if (name === "email") {
-      setEmail(value);
-    } else if (name === "password") {
-      setPassword(value);
-    }
+  // const onChange = (event) => {
+  //   const {
+  //     target: { name, value },
+  //   } = event;
+  //   if (name === "email") {
+  //     setEmail(value);
+  //   } else if (name === "password") {
+  //     setPassword(value);
+  //   }
+  // };
+
+  // const onSubmit = (event) => {
+  //   event.preventDefault();
+  // };
+
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm();
+
+  const {
+    register: signRegister,
+    handleSubmit: signHandleSubmit,
+    setError: signSetError,
+    formState: { errors: signErrors },
+  } = useForm();
+
+  const onLoginValid = (data) => {
+    console.log(data);
   };
 
-  const onSubmit = (event) => {
-    event.preventDefault();
+  const onSignUpValid = (data) => {
+    console.log(data);
   };
 
   return (
     <div>
-      <form onSubmit={onSubmit}>
+      <form
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          width: "250px",
+        }}
+        onSubmit={handleSubmit(onLoginValid)}
+      >
         <input
-          name="email"
-          type="text"
-          placeholder="Email"
-          required
-          value={email}
-          onChange={onChange}
+          {...register("loginId", {
+            required: "아이디를 작성해야 합니다.",
+          })}
+          placeholder="아이디"
         />
+        <span>{errors?.loginId?.message}</span>
         <input
-          name="password"
-          type="password"
-          placeholder="Password"
-          required
-          value={password}
-          onChange={onChange}
+          {...register("loginPw", {
+            required: "비밀번호를 작성해야 합니다.",
+            minLength: {
+              value: 2,
+              message: "비밀번호가 너무 짧습니다.",
+            },
+          })}
+          placeholder="비밀번호"
         />
-        <input type="submit" value="Log In" />
+        <span>{errors?.loginPw?.message}</span>
+
+        <button>확인</button>
       </form>
 
       <div>
         <button>Continue with Google</button>
         <button>Continue with GitHub</button>
       </div>
+
+      <form
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          width: "250px",
+        }}
+        onSubmit={signHandleSubmit(onSignUpValid)}
+      >
+        <input
+          {...signRegister("signUpId", {
+            required: "아이디를 작성해야 합니다.",
+          })}
+          placeholder="아이디"
+        />
+        <span>{signErrors?.signUpId?.message}</span>
+        <input
+          {...signRegister("signUpPw", {
+            required: "비밀번호를 작성해야 합니다.",
+            minLength: {
+              value: 5,
+              message: "비밀번호가 너무 짧습니다.",
+            },
+          })}
+          placeholder="비밀번호"
+        />
+        <span>{signErrors?.signUpPw?.message}</span>
+
+        <button>확인</button>
+      </form>
     </div>
   );
 }
