a17a7a78e1f84a17d51c5df52f659cc8f27c645a
ezurno <ezurno@gmail.com>
2023-05-24 20:19:21 +0900

    SET: firebase-database
    1. firebase 의 database 세팅
    
    2. firebase 공식 사이트로 가서 Cloud Store 를 사용해 연결함
    
    3. firebase/database 를 꼭 import 해야 함

--- src/fbase.js
+++ src/fbase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import "firebase/auth";
 import { getAuth } from "firebase/auth";
+import "firebase/database";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,28 @@
+import { useForm } from "react-hook-form";
+
 export default function Home() {
-  return <span>Home</span>;
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm();
+
+  const onValid = (data) => {
+    console.log(data.chat);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(onValid)}>
+        <input
+          {...register("chat", {
+            required: "내용을 작성해 주세요.",
+          })}
+          placeholder="chatting"
+        />
+        <button>Submit</button>
+      </form>
+    </>
+  );
 }
