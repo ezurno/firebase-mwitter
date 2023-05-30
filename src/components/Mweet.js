a743b66af75fb1af9f0abf1729672cc4d9d95ed3
ezurno <ezurno@gmail.com>
2023-05-30 21:32:08 +0900

    ADD: create delete
    1. window.comfirm 을 사용해 delete 여부를 사용자에게 되물음
    
    2. mweetTextRef 으로 해당하는 mweet 의 doc ref 를 지정 doc.(dbService, document, id)
    
    3. 비동기 처리를 걸어주어야함

--- src/components/Mweet.js
+++ src/components/Mweet.js
@@ -1,10 +1,27 @@
+import { dbService } from "fbase";
+import { deleteDoc, doc } from "firebase/firestore";
+
 export default function Mweet({ mweetObj, isOwner }) {
+  const onDeleteClick = async () => {
+    const ok = window.confirm("Are you sure you want to delete this mweet?");
+    console.log(ok);
+
+    const mweetTextRef = doc(dbService, "mweets", `${mweetObj.id}`);
+    // 해당 Meets 의 References
+
+    if (ok) {
+      // delete mweet
+      await deleteDoc(mweetTextRef);
+      // delete 실행, 비동기 처리해야함
+    }
+  };
+
   return (
     <>
       <h4>{mweetObj.text}</h4>
       {isOwner && (
         <>
-          <button>Delete</button>
+          <button onClick={onDeleteClick}>Delete</button>
           <button>Edit</button>
         </>
       )}
