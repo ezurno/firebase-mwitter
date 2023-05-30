c560bf6351e788e82e3280a01f3a363c966b2297
ezurno <ezurno@gmail.com>
2023-05-30 23:02:34 +0900

    ADD: create update
    1. update 버튼 구현
    
    2. updateDoc() 함수를 사용해 fireStore 에 update 요청
    
    3. react-hook-form 의 enter key 사용을 방지하기 위해 checkKeyDown 함수를 구현

--- src/components/Mweet.js
+++ src/components/Mweet.js
@@ -1,13 +1,25 @@
 import { dbService } from "fbase";
-import { deleteDoc, doc } from "firebase/firestore";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
+import { useState } from "react";
+import { set, useForm } from "react-hook-form";
 
 export default function Mweet({ mweetObj, isOwner }) {
-  const onDeleteClick = async () => {
-    const ok = window.confirm("Are you sure you want to delete this mweet?");
-    console.log(ok);
+  const [editing, setEditing] = useState(false);
+  const [targetValue, setTargetValue] = useState(mweetObj.text);
 
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+    setValue,
+  } = useForm();
+
+  const onDeleteClick = async () => {
     const mweetTextRef = doc(dbService, "mweets", `${mweetObj.id}`);
     // 해당 Meets 의 References
+    const ok = window.confirm("Are you sure you want to delete this mweet?");
+    console.log(ok);
 
     if (ok) {
       // delete mweet
@@ -16,13 +28,65 @@ export default function Mweet({ mweetObj, isOwner }) {
     }
   };
 
+  const toggleEditing = () => {
+    setEditing((prev) => !prev);
+  };
+
+  const onValid = async (data) => {
+    const mweetTextRef = doc(dbService, "mweets", `${mweetObj.id}`);
+
+    console.log(data);
+
+    await updateDoc(mweetTextRef, {
+      text: data.newMweet,
+    });
+
+    setEditing(false);
+  };
+
+  const onChangeMweet = async (event) => {
+    const {
+      target: { value },
+    } = event;
+    setTargetValue(value);
+  };
+
+  const checkKeyDown = (event) => {
+    if (event.key === "Enter") event.preventDefault();
+  };
+
   return (
     <>
-      <h4>{mweetObj.text}</h4>
-      {isOwner && (
+      {editing ? (
         <>
-          <button onClick={onDeleteClick}>Delete</button>
-          <button>Edit</button>
+          <form
+            onSubmit={handleSubmit(onValid)}
+            onKeyDown={(event) => checkKeyDown(event)}
+          >
+            <input
+              {...register("newMweet", {
+                required: "please write your comment.",
+              })}
+              type="text"
+              placeholder="Edit your comment."
+              value={targetValue}
+              onChange={onChangeMweet}
+            />
+
+            <span>{errors?.newMweet?.message}</span>
+            <button>Edit</button>
+          </form>
+          <button onClick={toggleEditing}>Cancel</button>
+        </>
+      ) : (
+        <>
+          <h4>{mweetObj.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>Delete</button>
+              <button onClick={toggleEditing}>Edit</button>
+            </>
+          )}
         </>
       )}
     </>
